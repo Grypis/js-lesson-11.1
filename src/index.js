6dfb44d32f2e73b3916ff8a28d90385dc9ed963d
grypis <grypisnecros@gmail.com>
2025-02-13 16:02:03 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+//* fetch(url, options)
 
+/* fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => {
+    // Response handling
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  }); */
+
+//!   =====================================
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Отримані дані: ', data);
+  })
+  .catch(error => {
+    console.log('Помилка запиту: ', error);
+  });
