3780901852b0b05a2f020aad67dbaf27363f83ae
grypis <grypisnecros@gmail.com>
2025-02-14 17:46:52 +0200

    asdgf

--- src/index.js
+++ src/index.js
@@ -27,3 +27,31 @@
   }); */
 
 //!   =====================================
+const options = {
+  method: 'GET',
+};
+
+fetch('https://jsonplaceholder.typicode.com/users', {
+  headers: {
+    Accept: 'application/json',
+  },
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
+
+/* fetch('some-url', {
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Custom-Header': 'custom value',
+    }
+  }) */
