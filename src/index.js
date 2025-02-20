c1479bdac89440d0d953995ac2dbea6e0d53d2c4
grypis <grypisnecros@gmail.com>
2025-02-20 16:20:03 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@
   }); */
 
 //!   =====================================
-const options = {
+/* const options = {
   method: 'GET',
 };
 
@@ -47,7 +47,7 @@ fetch('https://jsonplaceholder.typicode.com/users', {
   })
   .catch(error => {
     // Error handling
-  });
+  }); */
 
 /* fetch('some-url', {
   headers: {
@@ -55,3 +55,55 @@ fetch('https://jsonplaceholder.typicode.com/users', {
     'X-Custom-Header': 'custom value',
     }
   }) */
+
+//!   =====================================
+/* const fetchUserBtn = document.querySelector('.btn');
+const userList = document.querySelector('.user-list');
+
+fetchUserBtn.addEventListener('click', () => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(users => {
+      const markup = users
+        .map(user => {
+          return `<li>
+	          <p><b>Name</b>: ${user.name}</p>
+	          <p><b>Email</b>: ${user.email}</p>
+	          <p><b>Company</b>: ${user.company.name}</p>
+	        </li>`;
+        })
+        .join('');
+
+      userList.insertAdjacentHTML('beforeend', markup);
+    })
+    .catch(error => console.log(error));
+}); */
+
+//!   =====================================
+/* let globarVar;
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => {
+    console.log('users inside then callback: ', users);
+
+    globarVar = users;
+
+    console.log('globalVar inside fetch callback: ', globarVar);
+  });
+
+console.log('globarVar outside fetch: ', globarVar); */
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => {
+    console.log('Отримані дані: ', users);
+
+    renderUsers(users);
+  })
+  .catch(error => console.log('Error: ', error));
