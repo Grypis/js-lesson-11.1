2e98a8ac81c2006c4d76d086057430e7a5417a5d
grypis <grypisnecros@gmail.com>
2025-02-22 22:08:15 +0200

    as

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,38 @@ import './modules/ipfinder';
 import './modules/pokemon';
 import './modules/instagram';
 import './modules/user';
+
+//! ==========================================
+/* fetch('https://jsonplaceholder.typicode.com/comments?postId=1');
+fetch('https://jsonplaceholder.typicode.com/comments?postId=2');
+fetch('https://jsonplaceholder.typicode.com/comments?postId=3');
+fetch('https://jsonplaceholder.typicode.com/comments?postId=4');
+fetch('https://jsonplaceholder.typicode.com/comments?postId=5'); */
+
+//! ==========================================
+/* function foo(id) {
+  const BASE_URL = 'https://jsonplaceholder.typicode.com';
+  const END_POINT = '/comments';
+  const params = `?postId=${id}`;
+
+  const url = BASE_URL + END_POINT + params;
+  fetch(url);
+}
+
+foo(2);
+ */
+
+//! ==========================================
+function fetchUser(id) {
+  const BASE_URL = 'https://jsonplaceholder.typicode.com';
+  const END_POINT = '/users';
+  const params = new URLSearchParams({
+    id: id,
+  });
+
+  const url = `${BASE_URL}${END_POINT}?${params}`;
+
+  console.log(url);
+}
+
+fetchUser(1);
